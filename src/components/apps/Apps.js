e06f2300819c822736205a3378fcc2ab0ff4abc0
PrzemekWoznica <przemek.woznica2@gmail.com>
2021-08-03 01:50:10 +0200

    App ready

--- src/components/apps/Apps.js
+++ src/components/apps/Apps.js
@@ -3,8 +3,12 @@ import './Apps.css'
 
 function Apps() {
     return (
-        <section className="apps" id="apps">
-            
+        <section className='apps' id='apps'>
+            <div className='apps-content'>
+                <h3>Check out my apps</h3>
+                <a href='https://przemek-pets.herokuapp.com/' target='_blank' rel="noreferrer">Pets images</a>
+                <a href='https://przemek-todos.herokuapp.com/' target='_blank' rel="noreferrer">Todo App</a>
+            </div>
         </section>
     )
 }
