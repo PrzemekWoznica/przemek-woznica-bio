845c85342c6ebcc14a07b6fb4c49d4b20687107e
PrzemekWoznica <przemek.woznica2@gmail.com>
2021-08-03 16:59:44 +0200

    Add github link

--- src/components/apps/Apps.js
+++ src/components/apps/Apps.js
@@ -8,6 +8,7 @@ function Apps() {
                 <h3>Check out my apps</h3>
                 <a href='https://przemek-pets.herokuapp.com/' target='_blank' rel="noreferrer">Pets images</a>
                 <a href='https://przemek-todos.herokuapp.com/' target='_blank' rel="noreferrer">Todo App</a>
+                <a href='https://github.com/PrzemekWoznica' target='_blank' rel="noreferrer">My Github</a>
             </div>
         </section>
     )
